b945bdbf2c99f2f20cec6263e5787dad3c9b9a22
Samidha <samidha.j.1998@gmail.com>
2026-06-02 11:25:40 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,29 @@
-console.log("Game Loaded");
+let selectedSide = "";
+
+function selectSide(side) {
+  selectedSide = side;
+
+  alert(side === "groom" ? "Groom Side selected 🤵" : "Bride Side selected 👰");
+}
+
+function startGame() {
+  const playerName = document.getElementById("playerName").value;
+
+  if (selectedSide === "") {
+    alert("Please select Groom Side or Bride Side first.");
+    return;
+  }
+
+  if (playerName.trim() === "") {
+    alert("Please enter your player name.");
+    return;
+  }
+
+  document.getElementById("startScreen").style.display = "none";
+  document.getElementById("gameScreen").style.display = "block";
+
+  const icon = selectedSide === "groom" ? "🤵" : "👰";
+
+  document.getElementById("welcomeText").innerText =
+    "Welcome " + playerName + " " + icon;
+}
